f644af033a9783db6baf4ef601f038cd5b903921
Rahhi Barbool <42481759+BarboolRahhi@users.noreply.github.com>
2020-06-23 18:22:03 +0530

    add ApiResponse class

---
+++ src/main/java/com/example/springredditclone/dto/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.example.springredditclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private String message;
+}
